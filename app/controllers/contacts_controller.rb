5dec531be2190717bd27659ed8fbffad74d266e2
Kajetan Kaszowski <kajetan666@gmail.com>
2017-07-12 18:49:17 +0000

    Improved Error Messages

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,25 +1,22 @@
 class ContactsController < ApplicationController
-    def new
-      @contact = Contact.new
-    end
-    
-    def create
-      @contact = Contact.new(contact_params)
-      if @contact.save
-        redirect_to new_contact_path,
-        flash[:success] = "Message Sent"
-      else 
-        flash[:error] = @contact.errors.full_messages.join(", ")
-        redirect_to new_contact_path
-      end
-      
-    end
-    
-    
-  private
-  def contact_params
-    params.require(:contact).permit(:name, :email, :comments)
+def new
+  @contact = Contact.new
+end
+
+def create
+  @contact = Contact.new(contact_params)
+  if @contact.save
+    flash[:success] = "Message Sent"
+     redirect_to new_contact_path
+  else
+    flash[:error] = @contact.errors.full_messages.join(", ")
+     redirect_to new_contact_path 
+     
   end
-  
+end
+private
+  def contact_params
+     params.require(:contact).permit(:name, :email, :comments)
+  end
     
 end
